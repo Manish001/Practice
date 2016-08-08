b7258b23b117ada3dc8bcb036a17bee5204bad57
chauhan.manish <chauhan.manish@snapdeal.com>
2016-08-08 12:36:41 +0530

    Initialise repo.

---
+++ src/string/CheckOrderOfChar.java
@@ -0,0 +1,24 @@
+/**
+ @author chauhan.manish
+ * @Date   08-Aug-2016
+ * @Package Name string
+ * @Project codePractice
+ */
+package string;
+
+/**
+ *@author chauhan.manish
+ * @FileName CheckOrderOfChar.java
+ * @Time 12:36:08 PM
+ */
+public class CheckOrderOfChar {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
